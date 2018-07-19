cc3891841553d00ee37fc75c0e4d1fd183e18633
hanagemojio <hanagemojio@gmail.com>
2018-07-19 13:55:23 +0900

    Add user following

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -33,10 +33,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
 
-  test "micropost sidebar count" do
+  test "micropost, following and followers sidebar count" do
     log_in_as(@user)
     get root_path
     assert_match "#{@user.microposts.count} microposts", response.body
+    assert_match "#{@user.followers.count}", response.body
+    assert_match "#{@user.followers.count}", response.body
     # まだマイクロポストを投稿していないユーザー
     other_user = users(:malory)
     log_in_as(other_user)
